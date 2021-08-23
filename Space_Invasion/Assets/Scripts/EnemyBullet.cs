9a6568bd64bb4ad2bd82119d062d54f979eaebbb
Shikhar Garg <shikherbest5@gmail.com>
2021-08-23 19:56:19 +0530

    Update 3.0
    Enemy spawnning and Enemy bullet done

---
+++ Space_Invasion/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    float speed;
+    Vector2 _direction; //dirofbullet
+    bool isReady; //to know when bullet dir is set
+    private void Awake()
+    {
+        speed = 5f;
+        isReady = false;
+    }
+    public void SetDirection(Vector2 direction) //func to set bullet direction
+    {
+        _direction = direction.normalized;
+        isReady = true;
+    }
+    private void Update()
+    {
+        if (isReady)
+        {
+            Vector2 position = transform.position;
+            position += _direction * speed * Time.deltaTime;
+            transform.position = position;
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));//if the bullet is outside screen then remove
+            Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+            //Destroy bullet if goes out of bounds
+            if((transform.position.x < min.x) || (transform.position.x > max.x) ||
+                (transform.position.y < min.y) || (transform.position.y > max.y))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
---
+++ Space_Invasion/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyGO; //Enemy Prefab
+    public float maxSpawnRateInSeconds;
+    private void Start()
+    {
+        Invoke("SpawnEnemy", maxSpawnRateInSeconds);
+        InvokeRepeating("IncreaseSpawnRate", 0f, 50f);
+    }
+    void SpawnEnemy()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //Spawning at bottom left
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)); //Spawning at top right
+        GameObject anEnemy = (GameObject)Instantiate(EnemyGO);
+        //Instantiate Enemy
+        anEnemy.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+        ScheduleNextEnemySpawn();
+    }
+    void ScheduleNextEnemySpawn()
+    {
+        float spawnInSeconds;
+        if (maxSpawnRateInSeconds > 1f)
+        {
+            spawnInSeconds = Random.Range(1f, maxSpawnRateInSeconds); //picking a no. b/w 1 and maxspawnrateinseconds
+        }
+        else
+            spawnInSeconds = 1f;
+        Invoke("SpawnEnemy", spawnInSeconds);
+    }
+    void IncreaseSpawnRate() //Toincreasedifficulty
+    {
+        if (maxSpawnRateInSeconds > 1f)
+            maxSpawnRateInSeconds--;
+        if (maxSpawnRateInSeconds == 1f)
+            CancelInvoke("IncreaseSpawnRate");
+    }
+
+}
