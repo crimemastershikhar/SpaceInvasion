afcf651d0b409536682571629842badd37725e90
Shikhar Garg <shikherbest5@gmail.com>
2021-08-25 16:28:34 +0530

    Merge 2.0
    Unity demanded it

---
+++ Space_Invasion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Refrence to our game objects
+    public GameObject playButton;
+    public GameObject playerShip;
+    public GameObject enemySpawner;
+    public GameObject GameOverGo;
+    public GameObject scoreUITextGO;
+    public GameObject TimeCounterGO;
+    public GameObject GameTitileGO;
+/*    public GameObject ShootButton; // FOR ANDROID*/
+
+    public enum GameManagerState
+    {
+        Opening,
+        GamePlay,
+        GameOver,
+    }
+    GameManagerState GMState;
+    private void Start()
+    {
+        GMState = GameManagerState.Opening; 
+    }
+    void UpdateGameManagerState()
+    {
+        switch (GMState)
+        {
+            case GameManagerState.Opening:
+                playButton.SetActive(true);//Set Play button visible
+                GameOverGo.SetActive(false); //Hiding gameover screen
+                GameTitileGO.SetActive(true); //DIsplay game title
+                break;
+
+            case GameManagerState.GamePlay:
+                playButton.SetActive(false);
+                GameTitileGO.SetActive(false); //Hide Game title 
+                scoreUITextGO.GetComponent<GameScore>().Score = 0;
+                playerShip.GetComponent<PlayerControl>().Init();
+                enemySpawner.GetComponent<EnemySpawner>().ScheduleEnemySpawner();
+                TimeCounterGO.GetComponent<TimeCounter>().StartTimeCounter(); //Start the time counter
+/*                ShootButton.SetActive(true); // FOR ANDROID*/
+                break;
+
+            case GameManagerState.GameOver:
+                GameOverGo.SetActive(true);
+                enemySpawner.GetComponent<EnemySpawner>().UnscheduleEnemySpawner(); //stop enemy spawner
+                Invoke("ChangeToOpeningState", 4f);//change game managr state to openign state after 4 seconds
+                TimeCounterGO.GetComponent<TimeCounter>().StopTimeCounter(); //Stop time counter
+                /*ShootButton.SetActive(false); // FOR ANDROID*/
+                break;
+        }
+    }
+    public void SetGameManagerState(GameManagerState state)//Funct to set game manager state
+    {
+        GMState = state;
+        UpdateGameManagerState();
+    }
+    public void StartGamePlay()
+    {
+        GMState = GameManagerState.GamePlay;
+        UpdateGameManagerState();
+    }
+    public void ChangeToOpeningState()
+    {
+        SetGameManagerState(GameManagerState.Opening);
+    }
+}
---
+++ Space_Invasion/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject PlayerBulletGO;
+    public GameObject GameManagerGO;
+    public GameObject BulletPosition01;
+    public GameObject BulletPosition02 ;
+    public float speed;
+    public GameObject ExplosionGO;
+    public GameObject ExplosionPlayerGO;
+    public Text LivesUIText; //Refering to live UI text
+    const int MaxLives = 3; //max player per lives
+    int Lives; //curr player lives
+
+    public void Init()
+    {
+        Lives = MaxLives;
+        LivesUIText.text = Lives.ToString();// Update the lives UI Text
+        gameObject.SetActive(true); //Set player game object to active
+        transform.position = new Vector2(0, 0);//Reset the pos to centre
+    }
+    private void Start()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //bottom point corner of screen
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)); //top right point corner of screen
+        max.x = max.x - 0.225f; //subtracting player ship half width  
+        min.x = min.x + 0.225f; //add player ship half width
+        max.y = max.y - 0.285f; //subtracting player ship half height  
+        min.y = min.y + 0.285f; //add player ship half height
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //fire bullet
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+            //Instantiate and start first bullet position
+            GameObject bullet01 = (GameObject)Instantiate(PlayerBulletGO);
+            bullet01.transform.position = BulletPosition01.transform.position;
+            //Instantiate and start second bullet position
+            GameObject bullet02 = (GameObject)Instantiate(PlayerBulletGO);
+            bullet02.transform.position = BulletPosition02.transform.position;
+        }
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+            Vector2 direction = new Vector2(x, y).normalized;
+
+            Move(direction);
+        }
+        void Move(Vector2 direction)
+        {
+            Vector2 pos = transform.position; //find the position
+            pos += direction * speed * Time.deltaTime; //calculate position
+            transform.position = pos;//update player position
+        }
+        private void OnTriggerEnter2D (Collider2D col)
+        {
+            if ((col.tag == "EnemyShipTag") || (col.tag == "EnemyBulletTag"))
+            {
+                PlayPlayerExplosion();
+                Lives--;//Subtract one live
+                LivesUIText.text = Lives.ToString(); //Update lives UI Text
+                if (Lives == 0) // If player lives is 0
+                {
+                    //changing game manager stat to gameoverstate
+                    GameManagerGO.GetComponent<GameManager>().SetGameManagerState(GameManager.GameManagerState.GameOver);
+                    gameObject.SetActive(false); //Hide instead of destroy  
+                }
+            }
+        }
+        void PlayPlayerExplosion()
+        {
+            GameObject explosionplayer = (GameObject)Instantiate(ExplosionPlayerGO);
+            explosionplayer.transform.position = transform.position;
+        }
+    }
+
