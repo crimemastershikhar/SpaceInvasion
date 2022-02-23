5e4907836a3bd18eb12c1da11c15baf0604d8d61
Shikhar Garg <87324366+crimemastershikhar@users.noreply.github.com>
2022-02-23 18:24:16 +0530

    Update TimeCounter.cs

--- Space_Invasion/Assets/Scripts/TimeCounter.cs
+++ Space_Invasion/Assets/Scripts/TimeCounter.cs
@@ -14,7 +14,7 @@ public class TimeCounter : MonoBehaviour
     private void Start()
     {
         startCounter = false;
-        timeUI = GetComponent<Text>(); //fet text UI component from this gameobeject
+        timeUI = GetComponent<Text>(); 
     }
     public void StartTimeCounter()
     {
@@ -30,9 +30,9 @@ public class TimeCounter : MonoBehaviour
         if (startCounter)
         {
             ellapsedTime = Time.time - startTime;
-            minutes = (int)ellapsedTime / 60;  // get the mins
+            minutes = (int)ellapsedTime / 60;  
             seconds = (int)ellapsedTime % 60;
-            timeUI.text = string.Format("{0:00}:{1:00}", minutes, seconds); //update time counter UI text
+            timeUI.text = string.Format("{0:00}:{1:00}", minutes, seconds); 
         }
     }
 
