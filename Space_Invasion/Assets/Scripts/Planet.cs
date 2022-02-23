aeff93e0dedd2846c7de7776e89d7f92f155da4c
Shikhar Garg <87324366+crimemastershikhar@users.noreply.github.com>
2022-02-23 18:15:00 +0530

    Update Planet.cs

--- Space_Invasion/Assets/Scripts/Planet.cs
+++ Space_Invasion/Assets/Scripts/Planet.cs
@@ -13,8 +13,8 @@ public class Planet : MonoBehaviour
         isMoving = false;
         min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
         max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
-        max.y = max.y + GetComponent<SpriteRenderer>().sprite.bounds.extents.y; //add planet sprite to half height to max y
-        min.y = min.y - GetComponent<SpriteRenderer>().sprite.bounds.extents.y; //Subtract planet sprite to half height to max y
+        max.y = max.y + GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
+        min.y = min.y - GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
     }
     private void Update()
     {
@@ -23,7 +23,7 @@ public class Planet : MonoBehaviour
         Vector2 position = transform.position;
         position = new Vector2(position.x, position.y + speed * Time.deltaTime);
         transform.position = position;
-        if(transform.position.y < min.y) //if planet gets to min y pos then stop moving planet
+        if(transform.position.y < min.y)
         {
             isMoving = false;
         }
