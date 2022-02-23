b44a1c5adc947f13198e24bc058bb3c4384db5d7
Shikhar Garg <87324366+crimemastershikhar@users.noreply.github.com>
2022-02-23 18:20:03 +0530

    Update Star.cs

--- Space_Invasion/Assets/Scripts/Star.cs
+++ Space_Invasion/Assets/Scripts/Star.cs
@@ -7,14 +7,11 @@ public class Star : MonoBehaviour
     public float speed;
     private void Update()
     {
-        Vector2 position = transform.position; //Get pos of star
-        position = new Vector2(position.x, position.y - speed * Time.deltaTime); //compute new pos
-        transform.position = position; //update star pos
+        Vector2 position = transform.position; 
+        position = new Vector2(position.x, position.y - speed * Time.deltaTime);
+        transform.position = position; 
         Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
         Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
-
-        //if star gies out from bottom > pos star on top edge
-        //randpm spawn b/w left and right of screen
         if(transform.position.y < min.y)
         {
             transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
